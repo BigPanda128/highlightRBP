8369c11fe5fe44778c74828d7f65268da03381e6
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-04-14 22:01:26 -0400

    inline js code added here

---
+++ js/highlightAllCode.js
@@ -0,0 +1,19 @@
+var languages = {
+	javascript: /\b(function|return|var|if|else)\b/g /* ... etc */
+};
+
+function highlightAllCode() {
+	//gets what is in the tag pre and stores it in pres
+	var pres = document.body.getElementsByTagName("pre");
+	//iterate through the length of pres
+	for (var i = 0; i < pres.length; i++) {
+	//git pre that is in pres position i
+		var pre = pres[i];
+	        //git lang the atributres of the data language
+	        var lang = pre.getAttribute("data-language");
+	        //checks to see if it is special and in need of highlighitng
+	        	if (languages.hasOwnProperty(lang))
+	                //runs the highlight function to highlight the keyword
+	                highlightCode(pre, languages[lang]);
+	}
+}
